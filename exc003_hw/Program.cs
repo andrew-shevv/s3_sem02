b73a2c7c2c62ceed758f02683199ef007464bcc3
Andrew <vishnyaishere@gmail.com>
2022-08-22 02:40:57 +0300

    exc003 done

--- exc003_hw/Program.cs
+++ exc003_hw/Program.cs
@@ -1,3 +1,12 @@
 ﻿// Напишите программу, 
 // которая принимает на вход число 
 // и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.WriteLine("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Is the number divisible by both 7 and 23?");
+if (num % 23 == 0 && num % 7 == 0) {
+    Console.WriteLine("yes");
+}
+else Console.WriteLine("no");
