96c0b3882f1fa4e841df653e99dd34d49e54ec64
Andrew <vishnyaishere@gmail.com>
2022-08-22 02:26:16 +0300

    exc002 done

--- exc002/Program.cs
+++ exc002/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, 
+// которая будет принимать на вход два числа 
+// и выводить, является ли второе число кратным первому. 
+// Если число 2 не кратно числу 1, 
+// то программа выводит остаток от деления.
+
+Console.WriteLine ("Input first num: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Input second num: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 % num2 == 0){
+    Console.WriteLine($"{num1} is a multiple of {num2}");
+}
+else {
+    Console.WriteLine($"{num1} isn't a multiple of {num2}. The remainder of the division is {num1 % num2}");
+}
