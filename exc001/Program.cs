36c6c1ffb3ac772ebb1e84599e2e4a4691c9c96a
Andrew <vishnyaishere@gmail.com>
2022-08-22 02:10:24 +0300

    exc001 done

--- exc001/Program.cs
+++ exc001/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, 
+// которая выводит случайное трёхзначное число 
+// и удаляет вторую цифру этого числа.
+
+int num3dig = new Random().Next(100, 1000);
+Console.WriteLine("Randomly generated number: " + num3dig);
+
+int dig1 = num3dig / 100;
+int dig2 = num3dig % 10;
+
+Console.WriteLine($"{dig1}{dig2}");
