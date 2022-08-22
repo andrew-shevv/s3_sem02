eacbc9b53860b4e774244cf994f981d71ad9606a
Andrew <vishnyaishere@gmail.com>
2022-08-22 22:42:53 +0300

    exc006 done

--- exc006_hw/Program.cs
+++ exc006_hw/Program.cs
@@ -1,3 +1,14 @@
 ﻿// Напишите программу, 
 // которая выводит третью цифру заданного числа 
 // или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 99){
+    while (num > 999){
+        num = num / 10;
+    }
+    Console.WriteLine($"{num % 10} - third digit");
+}
+else Console.WriteLine("This number doesn't have a third digit");
