91b8d76c69ec4c78cb101bba95733072f89244f2
Andrew <vishnyaishere@gmail.com>
2022-08-22 22:12:12 +0300

    exc001 altered to fit the task more

--- exc001/Program.cs
+++ exc001/Program.cs
@@ -3,9 +3,10 @@
 // и удаляет вторую цифру этого числа.
 
 int num3dig = new Random().Next(100, 1000);
-Console.WriteLine("Randomly generated number: " + num3dig);
+Console.WriteLine($"Randomly generated number: {num3dig}");
 
 int dig1 = num3dig / 100;
 int dig2 = num3dig % 10;
+int num3digNoMid = dig1 * 10 + dig2;
 
-Console.WriteLine($"{dig1}{dig2}");
+Console.WriteLine(num3digNoMid);
