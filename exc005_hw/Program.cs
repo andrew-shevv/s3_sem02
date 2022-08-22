7c277fa20ae70c6ae5d1c22894c370406d157b79
Andrew <vishnyaishere@gmail.com>
2022-08-22 22:32:58 +0300

    exc005 done

--- exc005_hw/Program.cs
+++ exc005_hw/Program.cs
@@ -1,3 +1,12 @@
 ﻿// Напишите программу, 
 // которая принимает на вход трёхзначное число 
 // и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Input three-digit number: ");
+int num3dig = Convert.ToInt32(Console.ReadLine());
+
+int digMid = num3dig / 10 % 10;
+if (num3dig > 99 && num3dig < 1000){
+    Console.WriteLine($"{digMid} - middle digit");
+}
+else Console.WriteLine("This isn't a three-digit number");
