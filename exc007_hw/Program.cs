c9f3a46314017231c1291bde27cde40a0d27c822
Andrew <vishnyaishere@gmail.com>
2022-08-22 22:56:31 +0300

    exc007 done

--- exc007_hw/Program.cs
+++ exc007_hw/Program.cs
@@ -2,3 +2,14 @@
 // которая принимает на вход цифру, 
 // обозначающую день недели, 
 // и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Input number of weekday");
+int weekD = Convert.ToInt32(Console.ReadLine());
+
+if (weekD == 6 || weekD == 7){
+    Console.WriteLine("It's your day off!");
+}
+else if (weekD < 6 && weekD > 0){
+    Console.WriteLine("Time to work");
+}
+else Console.WriteLine("This isn't a weekday");
